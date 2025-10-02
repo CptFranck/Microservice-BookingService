0263eb374783c90b9bb8459bb04f432a0768ed84
CptFranck <francois.caillet71@gmail.com>
2025-10-02 19:57:36 +0200

    add bookingRequest

---
+++ src/main/java/com/CptFranck/BookingService/dto/BookingRequest.java
@@ -0,0 +1,19 @@
+package com.CptFranck.BookingService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingRequest {
+
+    private Long userId;
+
+    private Long eventId;
+
+    private Long ticketCount;
+}
