387ec75b552caa74047e5a2a8598f22a714bccaf
CptFranck <francois.caillet71@gmail.com>
2025-10-02 20:24:12 +0200

    add InventoryResponse

---
+++ src/main/java/com/CptFranck/BookingService/dto/InventoryResponse.java
@@ -0,0 +1,20 @@
+package com.CptFranck.BookingService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InventoryResponse {
+    private Long eventId;
+    private String event;
+    private Long capacity;
+    private VenueResponse venue;
+    private BigDecimal ticketPrice;
+}
