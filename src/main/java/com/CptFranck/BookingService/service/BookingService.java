b35873ab99d6fd00971aaa49f80cc9bf625c7c68
CptFranck <francois.caillet71@gmail.com>
2025-10-06 15:36:37 +0200

    finish createBooking method from BookingService

--- src/main/java/com/CptFranck/BookingService/service/BookingService.java
+++ src/main/java/com/CptFranck/BookingService/service/BookingService.java
@@ -1,23 +1,32 @@
 package com.CptFranck.BookingService.service;
 
 import com.CptFranck.BookingService.client.InventoryServiceClient;
+import com.CptFranck.BookingService.dto.BookingEvent;
 import com.CptFranck.BookingService.dto.BookingRequest;
 import com.CptFranck.BookingService.dto.BookingResponse;
 import com.CptFranck.BookingService.dto.InventoryResponse;
 import com.CptFranck.BookingService.entity.CustomerEntity;
 import com.CptFranck.BookingService.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
+@Slf4j
 public class BookingService {
 
     private final CustomerRepository customerRepository;
 
     private final InventoryServiceClient inventoryServiceClient;
 
-    public BookingService(com.CptFranck.BookingService.repository.CustomerRepository customerRepository, InventoryServiceClient inventoryServiceClient) {
+    private final KafkaTemplate<String, BookingEvent> kafkaTemplate;
+
+    public BookingService(com.CptFranck.BookingService.repository.CustomerRepository customerRepository, InventoryServiceClient inventoryServiceClient, KafkaTemplate<String, BookingEvent> kafkaTemplate) {
         this.customerRepository = customerRepository;
         this.inventoryServiceClient = inventoryServiceClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public BookingResponse createBooking(final BookingRequest request) {
@@ -25,12 +34,32 @@ public class BookingService {
                 .orElseThrow(() -> new RuntimeException("User not Found !"));
 
         final InventoryResponse inventoryResponse = inventoryServiceClient.getInventory(request.getEventId());
-        System.out.println("Inventory Service Response" + inventoryResponse);
+        log.info("Inventory Response : {}", inventoryResponse);
 
         if(inventoryResponse.getCapacity() < request.getTicketCount())
             throw (new RuntimeException("Not enough Tickets !"));
 
+        final BookingEvent bookingEvent = createBookingEvent(request, customer, inventoryResponse);
+        log.info("Booking Event : {}", bookingEvent);
 
-        return BookingResponse.builder().build();
+        kafkaTemplate.send("booking", bookingEvent);
+
+        return BookingResponse.builder()
+                .userId(bookingEvent.getUserId())
+                .eventId(bookingEvent.getEventId())
+                .ticketCount(bookingEvent.getTicketCount())
+                .totalPrice(bookingEvent.getTotalPrice())
+                .build();
+    }
+
+    private BookingEvent createBookingEvent(final BookingRequest request,
+                                            final CustomerEntity customer,
+                                            final InventoryResponse inventoryResponse) {
+        return BookingEvent.builder()
+                .userId(customer.getId())
+                .eventId(request.getEventId())
+                .ticketCount(request.getTicketCount())
+                .totalPrice(inventoryResponse.getTicketPrice().multiply(BigDecimal.valueOf(request.getTicketCount())))
+                .build();
     }
 }
